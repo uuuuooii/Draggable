d3c48733cebcb84cd506d92d1153cab47d063e9f
neo@zillinks.com <keunhey0107@gmail.com>
2022-11-13 19:10:43 +0900

    echo "# Draggable" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git r

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { useRef } from "react";
+import Draggable from "./lib/Draggable";
+
+function App() {
+  const handle = useRef(null);
+  return (
+    <Draggable handleRef={handle} onMove={(x, y) => console.log(x, y)}>
+      <div
+        style={{ width: "100px", height: "100px", background: "greenyellow" }}
+      >
+        <button ref={handle}>Move</button>
+      </div>
+    </Draggable>
+  );
+}
+
+export default App;
